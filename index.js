045ff61355460ff454da3f5a978c0f11444c4158
Mary <omota4fidelity@gmail.com>
2023-08-25 01:53:01 +0100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded");
+    const p = document.querySelector("#text");
+    p.textContent = "This is really cool!";
+});
+console.log("DOM not loaded");
